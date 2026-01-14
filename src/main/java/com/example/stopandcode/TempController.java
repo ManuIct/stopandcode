72ffeef6402c7ddefafd1a01b6a27ad946009ca3
Manu ICT <emanuelepalmitessa2018@gmail.com>
2026-01-14 09:05:41 +0100

    .

--- src/main/java/com/example/stopandcode/TempController.java
+++ src/main/java/com/example/stopandcode/TempController.java
@@ -9,12 +9,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class TempController {
 
     @GetMapping("/index")
-    public String converti(@RequestParam(name="temp", required = false, defaultValue = "0") double celsius, Model model) {
-        double fahrenheit = (celsius * 9/5) + 32;
+    public String converti(@RequestParam(name="temp", required = false, defaultValue = "0") String tempStr, Model model) {
+
+        double celsius;
+
+
+        String messaggioErrore = null;
+        try {
+
+            celsius = Double.parseDouble(tempStr.replace(",", "."));
+        } catch (Exception e) {
+            messaggioErrore = "Errore '" + tempStr + "' non è un numero. Inserisci un valore numerico.";
+            celsius = 0;
+
+        }
+
+
+        double fahrenheit = (celsius * 9 / 5.0) + 32;
         double kelvin = celsius + 273.15;
+
+
         model.addAttribute("celsius", celsius);
         model.addAttribute("fahrenheit", String.format("%.2f", fahrenheit));
         model.addAttribute("kelvin", String.format("%.2f", kelvin));
+        model.addAttribute("errore", messaggioErrore);
+
         return "index";
     }
 }
